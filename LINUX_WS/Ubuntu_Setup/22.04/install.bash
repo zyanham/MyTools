95f748a1a2e05da179df841f5fec711c4fe8cd37
zyanham <zyangalianhamster01@gmail.com>
2023-05-10 11:25:50 +0900

    update

--- LINUX_WS/Ubuntu_Setup/22.04/install.bash
+++ LINUX_WS/Ubuntu_Setup/22.04/install.bash
@@ -7,31 +7,44 @@ sudo apt install exfat-fuse exfatprogs
 
 ## apt command Package update
 sudo dpkg --add-architecture i386  
+sudo dpkg-reconfigure dash
 sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"  
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 40976EAF437D05B5
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
+
 sudo apt-get update
 sudo apt-get upgrade  
 
 ## For Petalinux etc..
-sudo apt-get -y install imagemagick vim git dos2unix iproute2 gawk make net-tools libncurses5-dev tftpd zlib1g  
-sudo apt-get -y install libssl-dev flex bison libselinux1 gnupg wget diffstat socat xterm autoconf libtool tar
-sudo apt-get -y install unzip texinfo zlib1g-dev gcc-multilib build-essential screen pax gzip python2.7 zlib1g:i386
-sudo apt-get -y install chrpath opencl-headers cmake libeigen3-dev libgtk-3-dev qt5-default freeglut3-dev
-sudo apt-get -y install libvtk6-qt-dev libtbb-dev ffmpeg libdc1394-22-dev libavcodec-dev libavformat-dev
-sudo apt-get -y install libswscale-dev libjpeg-dev libpng++-dev libtiff5-dev libopenexr-dev libwebp-dev libhdf5-dev
-sudo apt-get -y install libpython3.8-dev python3 python3-numpy python3-scipy python3-matplotlib libopenblas-dev
-sudo apt-get -y install liblapacke-dev python3-pip python3-dev pkg-config python3-h5py ipython3 graphviz python3-opencv
-sudo apt-get -y install cmake-qt-gui qtbase5-dev libopencv-dev docker docker.io libeigen3-dev cython3
-sudo apt-get -y install libcublas11 curl yasm libxine2-dev libv4l-dev swig libtinfo5 libtinfo6
+sudo apt-get -y install autoconf automake bison build-essential chrpath cmake cmake-qt-gui cpio curl cython3
+sudo apt-get -y install debianutils diffstat docker docker.io dos2unix dpkg-dev:i386 ffmpeg flex freeglut3-dev
+sudo apt-get -y install g++ gawk gcc gcc-multilib git gnupg google-perftools graphviz gzip haveged imagemagick
+sudo apt-get -y install iproute2 iputils-ping ipython3 lib32stdc++6 libavcodec-dev libavformat-dev libcublas11
+sudo apt-get -y install libdc1394-22-dev libegl1-mesa libeigen3-dev liberror-perl libfontconfig1:i386
+sudo apt-get -y install libgtk2.0-0:i386 libgtk-3-dev libhdf5-dev libjpeg-dev liblapacke-dev libncurses5
+sudo apt-get -y install libncurses5-dev libncurses5:i386 libncursesw5-dev libopenblas-dev libopencv-dev
+sudo apt-get -y install libopenexr-dev libpng++-dev libpython3.8-dev libsdl1.2-dev libselinux1 libsm6:i386
+sudo apt-get -y install libssl-dev libstdc++6:i386 libswscale-dev libtbb-dev libtiff5-dev libtinfo5 libtinfo6
+sudo apt-get -y install libtool libv4l-dev libvtk6-qt-dev libwebp-dev libx11-6:i386 libxcb-randr0-dev
+sudo apt-get -y install libxcb-shape0-dev libxcb-xinerama0-dev libxcb-xkb-dev libxcb-xtest0-dev libxext6:i386
+sudo apt-get -y install libxine2-dev libxrender1:i386 make mtd-utils ncurses-dev net-tools ocl-icd-libopencl1
+sudo apt-get -y install ocl-icd-opencl-dev opencl-headers openssh-server openssl pax perl pkg-config putty
+sudo apt-get -y install pylint python2.7 python3 python3-dev python3-git python3-h5py python3-jinja2 python3-matplotlib
+sudo apt-get -y install python3-numpy python3-opencv python3-pexpect python3-pip python3-scipy qt5-default qtbase5-dev
+sudo apt-get -y install screen socat swig sysvinit-utils tar texinfo tftpd tofrodos unzip util-linux vim wget xinetd
+sudo apt-get -y install xterm xtrans-dev xz-utils yasm zlib1g zlib1g-dev zlib1g:i386
 
 ## For Gstreamer
-sudo apt -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgtk2.0-dev libtbb-dev
-sudo apt -y install libatlas-base-dev libfaac-dev libmp3lame-dev libtheora-dev libvorbis-dev libxvidcore-dev
-sudo apt -y install libopencore-amrnb-dev libopencore-amrwb-dev x264 v4l-utils
-sudo apt -y install libprotobuf-dev protobuf-compiler libgoogle-glog-dev libgflags-dev libgphoto2-dev libeigen3-dev
-sudo apt -y install libhdf5-dev doxygen
-sudo apt -y install gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
-sudo apt -y install gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa
-sudo apt -y install gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+sudo apt -y install doxygen gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-libav
+sudo apt -y install gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-good
+sudo apt -y install gstreamer1.0-plugins-ugly gstreamer1.0-pulseaudio gstreamer1.0-qt5 gstreamer1.0-tools
+sudo apt -y install gstreamer1.0-x libatlas-base-dev libeigen3-dev libfaac-dev libgflags-dev
+sudo apt -y install libgoogle-glog-dev libgphoto2-dev libgstreamer1.0-dev libgstreamer-plugins-base1dsf.0-dev
+sudo apt -y install libgtk2.0-dev libhdf5-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt -y install libprotobuf-dev libtbb-dev libtheora-dev libvorbis-dev libxvidcore-dev protobuf-compiler
+sudo apt -y install v4l-utils x264 
+
 
 ## For Cmake
 sudo apt-get -y install libidn11 libidn11-dev
@@ -75,51 +88,3 @@ cd /usr/include/linux
 sudo ln -s -f ../libv4l1-videodev.h videodev.h
 cd ~
 
-#### Japanese Emv Setup
-##sudo apt install language-pack-ja-base language-pack-ja ibus-mozc
-##sudo update-locale LANG=ja_JP.UTF8
-##ibus restart
-##gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'jp'), ('ibus', 'mozc-jp')]"
-
-#### OpenCV Install
-###cd
-###mkdir OpenCV_Build
-###cd OpenCV_Build
-###git clone https://github.com/opencv/opencv.git -b 4.4.0
-###git clone https://github.com/opencv/opencv_contrib.git -b 4.4.0
-###mkdir opencv/build
-###cd opencv/build
-##
-###cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_C_COMPILER=/usr/bin/gcc -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_PYTHON_EXAMPLES=ON -D INSTALL_C_EXAMPLES=OFF -D WITH_TBB=ON -D WITH_CUDA=OFF -D BUILD_opencv_cudacodec=OFF -D ENABLE_FAST_MATH=1 -D CUDA_FAST_MATH=1 -D WITH_CUBLAS=1 -D WITH_V4L=ON -D WITH_QT=OFF -D WITH_OPENGL=ON -D WITH_GSTREAMER=ON -D OPENCV_GENERATE_PKGCONFIG=ON -D OPENCV_PC_FILE_NAME=opencv.pc -D OPENCV_ENABLE_NONFREE=ON -D OPENCV_PYTHON3_INSTALL_PATH=~/.virtualenvs/cv/lib/python3.8/site-packages -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.2.0/modules -D PYTHON_EXECUTABLE=~/.virtualenvs/cv/bin/python -D BUILD_EXAMPLES=ON ..
-##
-###sudo make -j7
-###sudo make install
-###sudo echo /usr/local/lib > /etc/ld.so.conf.d/opencv.conf
-###ldconfig -v
-###opencv_version
-##
-#### Github Clone
-##cd
-##mkdir Third_Party_GIT
-##mv Third_Party_GIT
-##git clone https://github.com/Digilent/vivado-library.git
-##git clone https://github.com/Digilent/vivado-boards.git
-##git clone https://github.com/Avnet/bdf.git
-##mv vivado-libary Digilent_IP
-##mv vivado-boards Digilent_bdf
-##mv bdf AVNET_bdf
-##
-##
-#### Vitis AI Setup
-####cd ~/Desktop
-####git clone -b v2.0 https://github.com/Xilinx/Vitis-AI.git
-####mv Vitis-AI Vitis-AI_v2.0
-####cd Vitis-AI_v2.0/setup/alveo
-####source ./install.sh
-####cd ../mpsoc/VART
-####./host_cross_compiler_setup.sh
-##
-##
-#### Docker TEX
-##sudo docker pull paperist/texlive-ja:latest
-##
