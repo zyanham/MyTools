bbf26f37bc7d4e4cb31de8b994e31beebcca5c69
zyanham <zyangalianhamster01@gmail.com>
2023-05-17 14:25:40 +0900

    update

---
+++ XILINX_WS/KRIA/KR260/v2022.1/Petalinux_Flow/README.md
@@ -0,0 +1,15 @@
+KR260 v2022.1 Vivado - Petalinux Flow   
+
+KR260 Setting  
+
+Refer to  
+[Getting Started with the Kria KR260 in Vivado 2022.1](https://www.hackster.io/whitney-knitter/getting-started-with-the-kria-kr260-in-vivado-2022-1-33746d)  
+[Getting Started with the Kria KR260 in PetaLinux 2022.1](https://www.hackster.io/whitney-knitter/getting-started-with-the-kria-kr260-in-petalinux-2022-1-daec16)  
+
+step1 tuning..  
+step2 never  
+step3 never  
+
+for Petalinux build  
+
+
---
+++ XILINX_WS/KRIA/KR260/v2022.1/Petalinux_Flow/step1_vivado/build/vivado/KR260_VivadoProj/KR260_VivadoProj.gen/sources_1/bd/kria_bd/hdl/kria_bd_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2022.1 (lin64) Build 3526262 Mon Apr 18 15:47:01 MDT 2022
+//Date        : Wed May 17 14:04:12 2023
+//Host        : meowth running 64-bit Ubuntu 18.04.4 LTS
+//Command     : generate_target kria_bd_wrapper.bd
+//Design      : kria_bd_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module kria_bd_wrapper
+   ();
+
+
+  kria_bd kria_bd_i
+       ();
+endmodule
