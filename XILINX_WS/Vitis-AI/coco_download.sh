e352a63486c1cd7cf71903dbe7ea0880f793f62a
zyanham <zyangalianhamster01@gmail.com>
2023-10-26 10:05:09 +0900

    update

---
+++ XILINX_WS/Vitis-AI/coco_download.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash 
+# Copyright 2020 Xilinx Inc.
+
+cd Download/
+
+#wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip -O annotations_trainval2017.zip
+#wget http://images.cocodataset.org/zips/train2017.zip -O train2017.zip
+#wget http://images.cocodataset.org/zips/val2017.zip -O val2017.zip
+#wget http://images.cocodataset.org/zips/test2017.zip -O test2017.zip
+
+unzip annotations_trainval2017.zip
+unzip train2017.zip
+unzip val2017.zip
+unzip test2017.zip
