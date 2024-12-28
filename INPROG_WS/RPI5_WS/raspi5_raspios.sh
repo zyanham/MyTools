f942499162ce61f9effb23eaaa7dad77d2ab0aea
zyanham <zyangalianhamster01@gmail.com>
2024-12-28 20:40:55 +0900

    update

--- INPROG_WS/RPI5_WS/raspi5_raspios.sh
+++ INPROG_WS/RPI5_WS/raspi5_raspios.sh
@@ -1,6 +1,13 @@
-mkdir -p ~/.config/pip
-echo "[global]" >> ~/.config/pip/pip.conf
-echo "break-system-packages = true" >> ~/.config/pip/pip.conf
+#mkdir -p ~/.config/pip
+#echo "[global]" >> ~/.config/pip/pip.conf
+#echo "break-system-packages = true" >> ~/.config/pip/pip.conf
+#
+#pip install opencv-python
+#
+##git clone https://github.com/onnx/models.git
 
-pip install opencv-python
+wget https://github.com/onnx/models/raw/refs/heads/main/validated/vision/body_analysis/ultraface/models/version-RFB-320.onnx
+wget https://github.com/onnx/models/raw/refs/heads/main/validated/vision/body_analysis/ultraface/models/version-RFB-640.onnx
+
+
 
