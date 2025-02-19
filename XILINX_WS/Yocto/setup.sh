e59840ed1c25a7a5a481be5ad8a7e02ab25cefda
zyanham <zyangalianhamster@gmail.com>
2025-02-19 15:01:48 +0900

    update

---
+++ XILINX_WS/Yocto/setup.sh
@@ -0,0 +1,3 @@
+sudo apt-get install -y gawk wget git-core diffstat unzip texinfo build-essential chrpath socat cpio python3 python3-pip 
+python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev 
+pylint xterm python3-subunit mesa-common-dev lz4 repo
---
+++ XILINX_WS/Yocto/v23.2_zcu102_min/start.bash
@@ -0,0 +1,5 @@
+repo init -u https://github.com/Xilinx/yocto-manifests.git -b rel-v2023.2
+repo sync
+repo start rel-v2023.2 --all
+source setupsdk
+
