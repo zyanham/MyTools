426677dce2fa98ea8865b089ad75c7bd1af8686d
zyanham <zyangalianhamster01@gmail.com>
2025-03-05 22:37:36 +0900

    update

--- AMD_WS/Yocto/try07_vek280_custom/start_vek280_wDDR2ch_generic.bash
+++ AMD_WS/Yocto/try07_vek280_custom/start_vek280_wDDR2ch_generic.bash
@@ -3,13 +3,13 @@ repo init -u https://github.com/Xilinx/yocto-manifests.git -b rel-v2024.2
 repo sync
 repo start rel-v2024.2 -all
 
-source setupsdk vek280_wDDR1ch-generic
+source setupsdk vek280_wDDR2ch-generic
 
 sed -i "/MACHINE ??=/c\MACHINE ??= \"versal-generic\"" conf/local.conf
 echo 'IMAGE_FSTYPES += "wic"' >>  conf/local.conf
 #echo 'require conf/machine/vek280-versal.conf' >> conf/local.conf
 echo 'require conf/machine/versal-generic.conf' >> conf/local.conf
 echo 'HDF_BASE = "file://"' >> conf/local.conf
-echo 'HDF_PATH = "$ROOT/vek280_wDDR1ch/system_wrapper.xsa"' >> conf/local.conf
+echo 'HDF_PATH = "$ROOT/vek280_wDDR2ch/system_wrapper.xsa"' >> conf/local.conf
 
 bitbake petalinux-image-minimal
